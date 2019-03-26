9de3375ab068d917e8e28bfd5ed43280d42873d5
Alexandr <arsenyukofficial@yandex.ru>
2019-03-26 17:18:19 +0300

    Initial commit

---
+++ MyVehicle/Profile/ConfirmCar/View/ConfirmCarViewInput.swift
@@ -0,0 +1,9 @@
+//
+//  ConfirmCarViewInput.swift
+//  MyVehicle
+//
+//  Created by Александр Арсенюк on 18/03/2019.
+//  Copyright © 2019 Александр Арсенюк. All rights reserved.
+//
+
+import Foundation
---
+++ MyVehicle/Profile/RegisterVehicle/Interactor/RegisterVehicleInteractorOutput.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterVehicleInteractorOutput.swift
+//  MyVehicle
+//
+//  Created by Александр Арсенюк on 18/03/2019.
+//  Copyright © 2019 Александр Арсенюк. All rights reserved.
+//
+
+import Foundation
